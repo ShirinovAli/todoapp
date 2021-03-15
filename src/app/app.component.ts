07d2a82de267f09a11ac5262f7425a40a25eace9
ali-shirinov@hotmail.com <ali-shirinov@hotmail.com>
2021-03-15 22:41:10 +0400

    model binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todoapp';
+  items = [
+    {
+      name: 'Anna',
+      action: 'no',
+    },
+    {
+      name: 'John',
+      action: 'no',
+    },
+    {
+      name: 'Larisa',
+      action: 'no',
+    },
+  ];
 }
